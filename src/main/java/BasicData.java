8205008f4bcda8bc1b0f86a70ed72623ede20948
lievre95 <ivandoga95@gmail.com>
2020-07-28 23:41:33 +0300

    data servlet>jsp

---
+++ src/main/java/BasicData.java
@@ -0,0 +1,31 @@
+// Import required java libraries
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class BasicData extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        //using context
+        ServletContext servletContext = getServletContext();
+        servletContext.setAttribute("name", "Jora");
+        servletContext.setAttribute("age",44);
+
+//        //data throw the session
+//        HttpSession session = request.getSession();
+//        session.setAttribute("name","newValue");
+//        session.setAttribute("age", 36);
+
+//        request.setAttribute("name", "Tom");
+//        request.setAttribute("age", "23");
+
+        getServletContext().getRequestDispatcher("/basic.jsp").forward(request, response);
+
+    }
+}
---
+++ src/main/webapp/basic.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>User Form</title>
+</head>
+<body>
+<body>
+<p>Name: ${name}</p>
+<p>Age: ${age}</p>
+</body>
+</html>
